5aeeae35abad0332493dcf1c360075860f741676
salvomc <smcrwrk@gmail.com>
2023-06-19 16:54:27 +0200

    bonus done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup>
+<script>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+export default {
+  components:
+  {
+    AppTitle,
+    AppHero,
+  }
+}
 </script>
 
 <template>
   <div>
     <AppTitle/>
+  </div>
+  <div>
+    <AppHero/>
   </div>
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="https://picsum.photos/500/300?grayscale" alt="Jumbotron">
+    </div>
+</template>
+
+<style scoped>
+    .jumbotron{
+        display: flex;
+        justify-content: center;
+    }
+    
+    img{
+        box-shadow: 10px 10px 5px #717171;
+    }
+</style>
