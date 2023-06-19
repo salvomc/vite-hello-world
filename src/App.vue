6163935207ef0bd11d18b86785ee574dfb0cf0b4
salvomc <smcrwrk@gmail.com>
2023-06-19 16:20:50 +0200

    base commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+</script>
+
+<template>
+  <div>
+    <AppTitle/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template scoped>
+    <div>
+       <h2>La mia prima app con Vite!</h2> 
+    </div>
+</template>
+
+<style scoped>
+    h2{
+        text-align: center;
+    }
+</style>
